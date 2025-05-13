50ccc2039635a7df1a14b090b815ede803053723
mobasshir <mobasshirkhan9931@gmail.com>
2025-05-13 14:51:30 +0530

    importing and exporting

---
+++ 1_import/import.js
@@ -0,0 +1,9 @@
+const { add , subtract, multiply } = require('./math.js'); // Importing functions from math.js
+// Using the imported functions
+//require is used to import modules in Node.js
+
+
+
+console.log(add(2, 3));
+console.log(subtract(5, 2));
+console.log(multiply(4, 3)); 
