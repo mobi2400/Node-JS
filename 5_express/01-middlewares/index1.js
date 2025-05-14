98554581aa248c362bb439a3c3646854bc52cd5f
mobasshir <mobasshirkhan9931@gmail.com>
2025-05-14 18:56:03 +0530

    custom middleware

--- 5_express/01-middlewares/index1.js
+++ 5_express/01-middlewares/index1.js
@@ -5,6 +5,7 @@ function logger(req, res, next) {
     console.log(`${req.method} ${req.url}`);
     next();
 }
+app.use(logger);
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
