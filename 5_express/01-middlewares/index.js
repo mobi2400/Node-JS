0769d6e188be88240d676eb5553112449f11ec0f
mobasshir <mobasshirkhan9931@gmail.com>
2025-05-14 18:34:48 +0530

    use of morgan middleWare

--- 5_express/01-middlewares/index.js
+++ 5_express/01-middlewares/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 import bodyParser from 'body-parser';
-
+import morgan from 'morgan';
 const app = express();
 const port = 3000;
+app.use(morgan('combined'));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
@@ -13,7 +14,7 @@ app.get('/', (req, res) => {
 app.post('/submit', (req, res) => {
     console.log(req.body);
     res.sendStatus(201);
-    
+
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
