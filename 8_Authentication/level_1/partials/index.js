efbacbf9b910bd010ceeb2085e0e326410526ebb
mobasshir <mobasshirkhan9931@gmail.com>
2025-05-27 23:54:27 +0530

    connected local database to the server

--- 8_Authentication/level_1/partials/index.js
+++ 8_Authentication/level_1/partials/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser"
+import pg from "pg";
+
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+  host: "localhost",
+  database: "Secret",
+  password: "Khan9931@#",
+  port: 5432,
+})
+db.connect()
 
 app.use(bodyParser.urlencoded({extende: true}))
 app.use(express.static("public"))
